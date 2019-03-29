603b83dbbaf5504543bae76a8143980d1bf4c734
Neven Skoro <nskoro@xiim.com>
2019-03-29 11:28:59 -0600

    initial commit

---
+++ Assets/BallControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D> ();
+        Invoke ("GoBall", 2);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    void GoBall () {
+        float rand = Random.Range (0, 2);
+        if (rand < 1) {
+            rb2d.AddForce (new Vector2 (20, Random.Range(-15, 15)) );
+        } else {
+            rb2d.AddForce (new Vector2 (-20, Random.Range(-15,15)) );
+        }
+    }
+
+    void ResetBall () {
+        rb2d.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+
+    void RestartGame () {
+        ResetBall ();
+        Invoke ("GoBall", 1);
+    }
+
+    void OnCollisionEnter2D (Collision2D coll) {
+        if (coll.collider.CompareTag("Player")) {
+            Vector2 vel;
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y) + (coll.collider.attachedRigidbody.velocity.y / 2);
+
+            if (vel.x < 1 && vel.x >-1)
+                vel.x = (vel.x * 2);
+            if (vel.y< 1 && vel.y >-1)
+                vel.y = vel.y * 2;
+
+            rb2d.velocity = vel;
+        }
+    }
+}
---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public bool computer = false;
+     
+    public float speed = 20.0f;
+    public float boundY = 0.25f;
+    private Rigidbody2D rb2d;
+    GameObject theBall;
+
+    // Start is called before the first frame update
+    void Start () {
+        Debug.Log("Start called.");
+        rb2d = GetComponent<Rigidbody2D>();
+        theBall = GameObject.FindGameObjectWithTag("Ball");
+    }
+
+    // Update is called once per frame
+    void Update () {
+      //   Debug.Log("Update called called.");
+        var vel = rb2d.velocity;
+
+        if (!computer)
+        { 
+            if (Input.GetKey (moveUp)) {
+                vel.y = speed;
+            } else if (Input.GetKey (moveDown)) {
+                vel.y = -speed;
+            } else {
+                vel.y = 0;
+            }
+        }
+        else
+        {
+            Debug.Log("computers turn to update " + theBall.transform.position.y);
+            if (rb2d.transform.position.y > (theBall.transform.position.y + 0.4))
+                vel.y = -speed;
+            else if (rb2d.transform.position.y < (theBall.transform.position.y-0.4))
+                vel.y = speed;
+            else
+                vel.y = 0;
+        }
+
+        rb2d.velocity = vel;
+
+
+        var pos = transform.position;
+        if (pos.y > boundY) {
+            pos.y = boundY;
+        } else if (pos.y < -boundY) {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
